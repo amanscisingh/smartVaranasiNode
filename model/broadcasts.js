aa518697276dd8c337061fdfda725882aab4ec21
Aman Singh <aman.singh.civ19@itbhu.ac.in>
2021-08-20 18:17:57 +0530

    ui fixes and optimization

---
+++ model/broadcasts.js
@@ -0,0 +1,26 @@
+import mongoose, { Collection } from 'mongoose';
+
+const Boradcasts = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    adminId: {
+        type: String,
+        required: true
+    },
+    views: {
+        type: Number,
+        default: 0
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    },   
+}, { collection : 'broadcasts' })
+
+export default mongoose.model('Broadcasts', Boradcasts);
